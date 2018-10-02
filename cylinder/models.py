cc4eabae974898265d35cdf2bf91727bf01221ba
shovan777 <raj.shrestha778@gmail.com>
2018-10-02 12:15:20 +0545

    Madness to sanity

--- cylinder/models.py
+++ cylinder/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Parameter(models.Model):
-    thickness = models.IntegerField(default=0)
+    # thickness = models.IntegerField(default=0)
+    name = models.CharField(max_length=255)
+    strength = models.FloatField(default=1)
