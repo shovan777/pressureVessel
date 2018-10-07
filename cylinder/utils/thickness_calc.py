ae58e777406bb4a2558c618310b9478244fedf95
shovan777 <raj.shrestha778@gmail.com>
2018-10-07 17:46:16 +0545

    Add thickness formula from div 1 for cylinder

--- cylinder/utils/thickness_calc.py
+++ cylinder/utils/thickness_calc.py
@@ -1,5 +1,10 @@
 """Calculate inner thickness."""
 from math import exp
 
-def cylinder_t(D, P, S, E=1.0):
-    return (D/2) * (exp(P / (S * E)) - 1)
+def cylinder_t(D, P, S, R, E=1.0, CA):
+    # return (D/2) * (exp(P / (S * E)) - 1
+    t_wo_allowance = (P * R) / (S * E - 0.6 * P)
+    t_w_allowance = t_wo_allowance + CA
+    return t_w_allowance
+
+# def conical_t()
