fc6570f6948b3e76a27d2c9e09c12648781e6cd7
Grunze1s <070bex446@ioe.edu.np>
2019-01-20 18:22:17 +0545

    token expires time is send to client

--- pressureVessel/utils.py
+++ pressureVessel/utils.py
@@ -4,5 +4,6 @@ from userAuth.serializers import UserSerializer
 def my_jwt_response_handler(token, user=None, request=None):
     return {
         'token': token,
-        'user': UserSerializer(user, context={'request': request}).data
+        'user': UserSerializer(user, context={'request': request}).data,
+        'expiresIn': 10
     }
