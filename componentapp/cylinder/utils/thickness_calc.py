dbaac20972b37fdc5f8b0d215f413470d36277d9
Grunze1s <070bex446@ioe.edu.np>
2019-02-10 17:11:37 +0545

    calculation changed to python calculation, serializer change to float

--- componentapp/cylinder/utils/thickness_calc.py
+++ componentapp/cylinder/utils/thickness_calc.py
@@ -7,16 +7,16 @@ def cylinder_t(P, S, D, C_A, E=1.0):
 
     Parameters
     ----------
-    P : float
-        Description of parameter `P`.
-    S : type
-        Description of parameter `S`.
-    R : type
-        Description of parameter `R`.
-    CA : type
-        Description of parameter `CA`.
-    E : type
-        Description of parameter `E`.
+    P : float psi
+        Design pressure or max allowable working pressure.
+    S : float psi
+        Stress value of material.
+    D : float inches
+        Inside diameter.
+    CA : float inches
+        Corrosion allowance.
+    E : float max 1.0
+        Joint Efficiency.
 
     Returns
     -------
@@ -29,10 +29,14 @@ def cylinder_t(P, S, D, C_A, E=1.0):
     # t_wo_allowance = (P * R) / (S * 1000 * E - 0.6 * P)
     # t_w_allowance = t_wo_allowance + C_A
     # return t_w_allowance
-    with connection.cursor() as cursor:
-        cursor.callproc('cylinder_t', [P, S, D, C_A, E])
-        return cursor.fetchall()[0][0]
 
+    # Process to calculate using Postgres Procedure
+    # with connection.cursor() as cursor:
+    #     cursor.callproc('cylinder_t', [P, S, D, C_A, E])
+    #     return cursor.fetchall()[0][0]
+    upper_part = float(P * ( D/2 ) )
+    lower_part = float((S * E) - (0.6 * P))
+    return (upper_part/lower_part) + C_A
 
 def conical_t(D, P, S, D_l, D_s, L_c, CA, E=1.0):
     D_l += 2 * CA
