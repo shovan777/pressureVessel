6d4ae964aecab5ad266c49227492ca02dff0cb26
shovan777 <raj.shrestha778@gmail.com>
2020-01-27 12:58:50 +0545

    Add db configs and cloudsql proxy in deployment manifest. Change db to use postgresql.

--- pressureVessel/settings.py
+++ pressureVessel/settings.py
@@ -129,18 +129,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pressureVessel.wsgi.application'
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'HOST': os.environ['DB_HOST'],
-    #     'PORT': os.environ['DB_PORT'],
-    #     'NAME': os.environ['DB_NAME'],
-    #     'USER': os.environ['DB_USER'],
-    #     'PASSWORD': os.environ['DB_PASSWORD']
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD']
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
